11852b9be3e1def60b0c604270fd01930517740a
Alex Nuthutch <knigotex@gmail.com>
2024-04-29 01:32:37 +0500

    update requirements and set_menu

--- keyboards/set_menu.py
+++ keyboards/set_menu.py
@@ -4,8 +4,8 @@ from fluentogram import TranslatorRunner
 
 
 # Функция для настройки кнопки Menu бота
-async def set_main_menu(bot: Bot, i18n: TranslatorRunner):
-    commands = {"/models": i18n.change.model()}
+async def set_main_menu(bot: Bot):
+    commands = {"/models": "Сменить модель"}
     main_menu_commands = [
         BotCommand(command=command,
                    description=description) for command,
