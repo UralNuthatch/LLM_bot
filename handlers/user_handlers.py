174796a86882ef9db85d1382ce9b994d6e045b02
Alex Nuthutch <knigotex@gmail.com>
2024-05-08 16:03:01 +0500

    fixed voice_prompt

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -60,9 +60,9 @@ async def clear_last_messages(message: Message, i18n: TranslatorRunner, last_mes
     await message.answer(i18n.cleared.cache())
 
 
-# Этот хэндлер срабатывает когда приходит звуковое сообщение
-@router.message(F.voice.as_("voice_prompt"))
-async def get_send_audio(message: Message, voice_prompt: Voice, bot: Bot, i18n: TranslatorRunner, db: DB):
+# Этот хэндлер срабатывает когда приходит звуковое сообщение, а в ответ будет текст
+@router.message(F.voice.as_("voice_prompt"), TextResponse())
+async def send_audio_text(message: Message, voice_prompt: Voice, bot: Bot, i18n: TranslatorRunner, db: DB, llm: dict, last_messages: list):
     try:
         # Скачивание файла в ogg формате
         file = await bot.get_file(voice_prompt.file_id)
@@ -80,27 +80,87 @@ async def get_send_audio(message: Message, voice_prompt: Voice, bot: Bot, i18n: 
         # Преобразование аудиозаписи в текст
         text = recognizer.recognize_google(audio, language="ru-RU")
 
-        await message.answer(f"{i18n.processing.request()} {text}")
+        await message.answer(f"{llm["llm_img"]} {llm["llm_name"]}:\n{i18n.processing.request()} {text}")
 
-        # Определяем по БД модель ИИ пользователя
-        llm_category, llm_model, llm_name, llm_img, llm_response = await db.get_users_llm(message.from_user.id)
+        # Добавляем запрос в последние сообщения
+        last_messages.append({"role": "user", "content": str(text)})
+
         # Обрабатываем запрос в зависимости от модели
-        response = await select_model_category(llm_category, llm_model, str(text), message.from_user.id, db)
-        # Если был запрос на генерацию изображения
-        if response == 'image':
-            if os.path.exists(f"{message.chat.id}.jpeg"):
-                img = FSInputFile(f"{message.chat.id}.jpeg")
-                await message.answer_photo(img)
-                os.remove(f"{message.chat.id}.jpeg")
-        elif response == 'no_key':
-            await message.answer("К сожалению закончились попытки генерации изображений. Попробуйте позднее.")
-            await message.bot.send_message(chat_id=348123497, text='закончились ключи для stability')
-        else:
-            await message.answer(f"{llm_img} {llm_name}:\n{response}")
+        response = await select_model_category(llm["llm_category"], llm["llm_model"], str(text), message.from_user.id, db, last_messages)
+
+        # Если длина сообщения больше 4096 - исключение TelegramBadRequest - message too long
+        await message.answer(f'{llm["llm_img"]} {llm["llm_name"]}:\n{response[:4050]}')
+        if response[4050:]:
+            await message.answer(f'{response[4050:]}')
+        # Добавляем ответ в последние сообщения чтобы сохранить в Redis
+        last_messages.append({"role": "assistant", "content": response})
+
+    # Если parse_mode=Markdown поломан - TelegramBadRequest - can't parse entities...
+    except TelegramBadRequest:
+        await message.answer(f'{llm["llm_img"]} {llm["llm_name"]}:\n{response[:4050]}', parse_mode='HTML')
+        if response[4050:]:
+            await message.answer(f'{response[4050:]}', parse_mode='HTML')
+        # Добавляем ответ в последние сообщения
+        last_messages.append({"role": "assistant", "content": response})
 
     except Exception as ex:
-        await message.answer(i18n.error())
+        # Удаляем последний запрос из последних сообщений, т.к. не получили ответа
+        last_messages.pop()
         logging.error(ex)
+        await message.answer(f'{llm["llm_img"]} {llm["llm_name"]}:\n{i18n.error()}\n{i18n.clear.cache()}')
+
+    finally:
+        # Удаление скачанного ogg и преобразованного в wav файлов
+        if os.path.exists(f"{message.chat.id}.ogg"):
+            os.remove(f"{message.chat.id}.ogg")
+        if os.path.exists(f"{message.chat.id}.wav"):
+            os.remove(f"{message.chat.id}.wav")
+
+
+
+# Этот хэндлер срабатывает когда приходит звуковое сообщение, а в ответ будет изображение
+@router.message(F.voice.as_("voice_prompt"), ImgResponse())
+@flags.chat_action("upload_photo")
+async def send_audio_img(message: Message, voice_prompt: Voice, bot: Bot, i18n: TranslatorRunner, db: DB, llm: dict):
+    try:
+        # Отправляем статус печатает
+        await message.bot.send_chat_action(message.chat.id, "typing")
+        # Скачивание файла в ogg формате
+        file = await bot.get_file(voice_prompt.file_id)
+        file_path = file.file_path
+        await bot.download_file(file_path, f"{message.chat.id}.ogg")
+
+        # Конвертация в wav формат
+        data, samplerate = soundfile.read(f"{message.chat.id}.ogg")
+        soundfile.write(f"{message.chat.id}.wav", data, samplerate)
+
+        recognizer = sr.Recognizer()
+        with sr.AudioFile(f"{message.chat.id}.wav") as source:
+            audio = recognizer.record(source=source)
+
+        # Преобразование аудиозаписи в текст
+        text = recognizer.recognize_google(audio, language="ru-RU")
+
+        await message.answer(f'{llm["llm_img"]} {llm["llm_name"]}:\n{i18n.processing.request()} {text}')
+
+        # Обрабатываем запрос в зависимости от модели
+        await select_model_category(llm["llm_category"], llm["llm_model"], str(text), message.from_user.id, db)
+
+        if os.path.exists(f"{message.chat.id}.png"):
+            img = FSInputFile(f"{message.chat.id}.png")
+            if os.path.getsize(f"{message.chat.id}.png") < 10485760:
+                await message.answer_photo(img)
+            await message.answer_document(img)
+            os.remove(f"{message.chat.id}.png")
+    # Если закончились ключи
+    except NoKeyError:
+        await message.answer(i18n.keys.ended())
+        await send_no_key(bot)
+    # Обрабатываем другие ошибки
+    except Exception as ex:
+        logging.error(ex)
+        await message.answer(f'{llm["llm_img"]} {llm["llm_name"]}:\n{i18n.error()}')
+
     finally:
         # Удаление скачанного ogg и преобразованного в wav файлов
         if os.path.exists(f"{message.chat.id}.ogg"):
